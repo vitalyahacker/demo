0aa6e52917ccca6cc077f8e22dfba190a02e549a
supervitalya <vitalyatrololo@mail.ru>
2019-10-21 23:22:10 +0300

    Fix controller

--- src/main/java/com/vitalya/demo/controller/DemoController.java
+++ src/main/java/com/vitalya/demo/controller/DemoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 
-@RestController("/demo")
+@RestController
 public class DemoController {
 
     @GetMapping
