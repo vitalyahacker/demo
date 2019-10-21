0b9810bf0e104d8d7fd90be633744bab18f5563b
supervitalya <vitalyatrololo@mail.ru>
2019-10-21 23:26:21 +0300

    Add one more method

--- src/main/java/com/vitalya/demo/controller/DemoController.java
+++ src/main/java/com/vitalya/demo/controller/DemoController.java
@@ -1,6 +1,7 @@
 package com.vitalya.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -12,5 +13,10 @@ public class DemoController {
     @GetMapping
     public List<String> findAll() {
         return Arrays.asList("Vova", "Vitya", "Vitalya");
+    }
+
+    @PostMapping
+    public String save(String s){
+        return s;
     }
 }
